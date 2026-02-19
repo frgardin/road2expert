9946977ea4b81bf10a60b9e7cd857f790a64abeb
Felipe Rezende Gardin <gardinfelipe@gmail.com>
2026-02-18 22:55:42 -0300

    learning

--- src/getcharputchar/hello.c
+++ src/getcharputchar/hello.c
@@ -1,10 +1,17 @@
 #include <stdio.h>
 
 int main() {
-    int ch;
+    char ch;  // Variable to store the character input
 
+    // Ask the user to enter a character
+    printf("Input some char: ");
+    // Use getchar() to read a single character
     ch = getchar();
 
-    putchar(ch);
+    // Use putchar() to print the entered character
+    printf("You entered: ");
+    putchar(ch);  // Display the entered character
+    printf("\n");  // Move to the next line after output
+
     return 0;
 }
