735b136100a8fa4808f558b812b0ba529b4ff9ac
Felipe Rezende Gardin <gardinfelipe@gmail.com>
2026-02-24 07:33:59 -0300

    template

---
+++ src/codechef/practice/zig-zag-traversal.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+struct Node {
+	int val;
+	struct Node* left;
+	struct Node* right;
+};
+
+void zigzagTraversal(struct Node* root) {
+
+}
+
+
+
+/*
+*
+*5
+*1 2 L
+*1 3 R
+*3 4 L
+*3 5 R
+*/
+int main() {
+	int n; scanf("%d", &n);
+	
+}
