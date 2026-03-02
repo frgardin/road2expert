5a685ee58b458457baf4c792031edc94a42dc514
Felipe Rezende Gardin <gardinfelipe@gmail.com>
2026-03-02 19:51:04 -0300

    finally

--- src/codechef/dp/final-solution-max-sub-set-array.cpp
+++ src/codechef/dp/final-solution-max-sub-set-array.cpp
@@ -10,14 +10,29 @@ int dp(int* a, int i) {
     return max(a[i] + dp(a, i-2), dp(a, i-1));
 }
 
+int dp(int* a, int i, int n, int* mem) {
+    if (i < n) {
+        mem[i] = max(a[i] + mem[i-2], mem[i-1]);
+        return dp(a, i+1, n, mem);
+    }
+    return mem[n-1];
+}
+
 int main() {
     int t; cin >> t;
     while (t--) {
         int n; cin >> n;
         int a[n];
+        int mem[n];
         for (int i =0;i<n;i++) {
             cin >> a[i];
+            mem[i] = 0;
         }
-        cout << dp(a, n-1) << endl;
+        
+        mem[0] = max(a[0], 0);
+        mem[1] = max(a[1], mem[0]);
+
+        // cout << "slow dp: " << dp(a, n-1) << endl;
+        cout << dp(a, 2, n, mem) << endl;
     }
 }
