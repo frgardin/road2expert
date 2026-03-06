82aee49f096ddf90f67e156e178db48cd9a8a5e7
Felipe Rezende Gardin <gardinfelipe@gmail.com>
2026-03-05 22:49:23 -0300

    asd

---
+++ src/codechef/practice/farenheit-to-celsius.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main() {
+    float fahrenheit = 98.3;
+    // Complete the code
+    float celsius = (fahrenheit-32.0) * 5.0/9.0;
+    printf("%f", celsius);
+    return 0;
+}
