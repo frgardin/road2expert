eb67a25fe634e653458d0ed27f93f9608023aee4
Felipe Rezende Gardin <gardinfelipe@gmail.com>
2026-03-17 19:26:35 -0300

    simple calculatro

---
+++ src/codeforces/simple-calculator.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+typedef long long ll;
+
+int main() {
+    ll a, b; cin >> a >> b;
+    cout << a << " + " << b << " = " << a+b << endl;
+    cout << a << " * " << b << " = " << a*b << endl;
+    cout << a << " - " << b << " = " << a-b << endl; 
+}
