b2f2c26fa1ea40e49744eeb0b6c0e0caeda272b1
Felipe Rezende Gardin <gardinfelipe@gmail.com>
2026-03-17 23:19:15 -0300

    trying the last 2 digits

---
+++ src/codeforces/the-last-2-digits.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+typedef long long ll;
+
+int main() {
+    ll a, b, c, d; cin >> a >> b >> c >> d;
+
+    ll am = a % 1000;
+    ll bm = b % 1000;
+    ll cm = c % 1000;
+    ll dm = d % 1000;
+    ll m = 100;    
+    
+    string ans = to_string((am*bm*cm*dm));
+
+    if (ans.size() > 1) {
+        cout << ans.at(ans.size()-2) << ans.at(ans.size()-1) << endl;
+    } else {
+        cout << ans.at(ans.size()-1) << endl;
+    }
+}
