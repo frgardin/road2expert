f8c15c2b9845c4ef292674f589c5b2dcd40364cf
Felipe Rezende Gardin <gardinfelipe@gmail.com>
2026-03-26 19:48:44 -0300

    one prime

--- src/codeforces/is-prime.c
+++ src/codeforces/is-prime.c
@@ -1,23 +1,20 @@
 #include <stdio.h>
+#include <math.h>
+
+int isPrime(int n) {
+    if (n < 2) return 0;
+    if (n == 2) return 1;
+    if (n % 2 == 0) return 0;
+
+    int limit = (int) sqrt((double) n);
+    for (int i =3;i<=limit;i++) {
+        if (n % i == 0) return 0;
+    }
+    return 1;
+}
+
 
 int main() {
     int n; scanf("%d", &n);
-    if (n == 2) {
-        printf("YES\n");
-        return 0;
-    }
-    if (n % 2 == 0) {
-        printf("NO\n");
-        return 0;
-    }
-    int i =3;
-    while (n % i != 0 && i < n) {
-        i++;
-    }
-    if (i == n ) {
-        printf("YES\n");
-        return 0;    
-    }
-    printf("NO\n");
-    return 0;
+    printf((isPrime(n) ? "YES\n" : "NO\n"));
 }
