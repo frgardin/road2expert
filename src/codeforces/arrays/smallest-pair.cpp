46cea95975dc17edee22eac728fd5dce7cd64bba
Felipe Rezende Gardin <gardinfelipe@gmail.com>
2026-04-05 23:53:33 -0300

    coding

--- src/codeforces/arrays/smallest-pair.cpp
+++ src/codeforces/arrays/smallest-pair.cpp
@@ -1,29 +1,32 @@
 #include <iostream>
+#include <vector>
+#include <cmath>
 
 using namespace std;
 
 typedef long long ll;
 
-ll smallestPair(int a[], int& n) {
-    ll sum = a[0] + a[1] + 1 - 0;
-    for (int i = 1;i<n-1;i++) {
-        for (int j = i+1;j<n;j++) {
-            ll p = a[i]+a[j]+j-i;
-            if (p < sum) {
-                sum=p;
-            }
+ll smallestPair(vector<int> v, int n)
+{
+    ll sum = v[0] + v[1] + 1;
+    for (int i = 0; i < n - 1; i++)
+    {
+        for (int j = i + 1; j < n; j++)
+        {
+            sum = fmin(v[i]+v[j]+j-i, sum);
         }
     }
-
     return sum;
 }
 
-
-int main() {
+int main()
+{
     int t; cin >> t;
-    while (t--) {
-    int n; cin >> n;
-    int a[n];
-    for (int& i : a) cin >> i;
-    cout << smallestPair(a, n) << endl; }
+    while (t--) 
+    {
+        int n; cin >> n;
+        vector<int>v(n);
+        for (auto &i:v) cin >> i;
+        cout << smallestPair(v, n) << endl;
+    }
 }
