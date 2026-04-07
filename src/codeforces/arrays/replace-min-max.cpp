f61e57c68d928011de1cce4e9ca717c0c64d07eb
Felipe Rezende Gardin <gardinfelipe@gmail.com>
2026-04-06 23:50:49 -0300

    coding

--- src/codeforces/arrays/replace-min-max.cpp
+++ src/codeforces/arrays/replace-min-max.cpp
@@ -1,56 +1,20 @@
-#include <iostream>
-#include <algorithm>
-#include <vector>
+#include <iostream>   // cin, cout
+#include <vector>     // vector
+#include <algorithm>  // min_element, max_element, iter_swap
 
 using namespace std;
 
 typedef vector<int> vi;
 
-
-int minVector(vi &v)
-{
-    int minValue = 1000000;
-    int ans=-1;
-    for (int i = 0;i<v.size();i++) 
-    { 
-        if (minValue > v[i]) 
-        {
-            minValue = v[i];
-            ans=i;
-        }
-    }
-    return ans;
-}
-
-
-int maxVector(vi &v)
-{
-    int maxValue = -1000000;
-    int ans=-1;
-    for (int i = 0;i<v.size();i++) 
-    { 
-        if (maxValue < v[i]) 
-        {
-            maxValue = v[i];
-            ans=i;
-        }
-    }
-    return ans;
-}
-
-void swap(vi &v, int i, int j)
-{
-    int aux = v[i];
-    v[i] = v[j];
-    v[j] = aux;
-}
-
 int main()
 {
     int n; cin >> n;
     vi v(n);
-    for (int &i : v) cin >> i;
-    swap(v, minVector(v), maxVector(v));
-    for (int i : v) cout << i << ' ' ;
-    cout << endl; 
+    for (int &x : v) cin >> x;
+
+    iter_swap(min_element(v.begin(), v.end()),
+              max_element(v.begin(), v.end()));
+
+    for (int x : v) cout << x << ' ';
+    cout << '\n';
 }
