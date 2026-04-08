cf04383d5beeee0e992419b9c8c407740f4b1985
Felipe Rezende Gardin <gardinfelipe@gmail.com>
2026-04-08 11:35:41 -0300

    minimize number

--- src/codeforces/arrays/minimize-number.cpp
+++ src/codeforces/arrays/minimize-number.cpp
@@ -1,24 +1,49 @@
 #include <iostream>
-#include <set>
+#include <unordered_set>
 #include <algorithm>
-#include <unordered_map>
+#include <vector>
 
 using namespace std;
 
-typedef set<int> si;
-typedef unordered_map<int> umi;
+typedef vector<int> vi;
 
-int[] divisors = {1, 2, 4, 8, 16, 32, 64, 128, 256, 512, 1024, 2048, 4096, 8192, 16384, 32768, 65536, 131072, 262144, 524288, 1048576, 2097152, 4194304, 8388608, 16777216, 33554432, 67108864, 134217728, 268435456, 536870912};
+vi divisors = {1, 2, 4, 8, 16, 32, 64, 128, 256, 512, 1024, 2048, 4096, 8192, 16384, 32768, 65536, 131072, 262144, 524288, 1048576, 2097152, 4194304, 8388608, 16777216, 33554432, 67108864, 134217728, 268435456, 536870912};
 
-int minimizeNumber(si s)
+int find(int &n)
+{
+    int l = 0;
+    int r = divisors.size() - 1;
+    while (l < r)
+    {
+        int mid = (l + r + 1) / 2;
+        if (n % divisors[mid] == 0)
+        {
+            l = mid;
+        }
+        else
+        {
+            r = mid - 1;
+        }
+    }
+    return l;
+}
+
+int minimizeNumber(vi &v)
 {
     int ans = 1000000001;
+    for (int i : v)
+    {
+        ans = min(find(i), ans);
+    }
+    return ans;
 }
 
 int main()
 {
-    int n;cin >> n;
-    si s(n);
-    for (int &i : s) cin >> i;
-    cout << minimizeNumber(s) << endl;
+    int n;
+    cin >> n;
+    vi v(n);
+    for (int &i : v)
+        cin >> i;
+    cout << minimizeNumber(v) << endl;
 }
