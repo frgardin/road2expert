9c99ad20d05770f4df4b32f31dabcc0c72804c17
Felipe Rezende Gardin <gardinfelipe@gmail.com>
2026-04-14 19:52:02 -0300

    way-too-long-words

--- src/codeforces/strings/way-too-long-words.cpp
+++ src/codeforces/strings/way-too-long-words.cpp
@@ -13,7 +13,7 @@ int main() {
         if (s.size() <= 10) {
             cout << s << endl;
         } else {
-            cout << s[0] << s.size()-2 << s[s.size()-1] <<endl;
+            cout << s[0] << s.size()-2 << s.back() <<endl;
         }
     }
 }
