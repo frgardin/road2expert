b14d7a095e56a352b25b3a3e0bafcf556372309c
Felipe Rezende Gardin <gardinfelipe@gmail.com>
2026-04-14 19:51:22 -0300

    way-too-long-words

---
+++ src/codeforces/strings/way-too-long-words.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+    int n; cin >> n;
+    cin.ignore();
+    while (n--) {
+        string s;
+        getline(cin, s);
+        
+        if (s.size() <= 10) {
+            cout << s << endl;
+        } else {
+            cout << s[0] << s.size()-2 << s[s.size()-1] <<endl;
+        }
+    }
+}
