1661939b224789dfefd8f85dd48847d60ba7af0b
Felipe Rezende Gardin <gardinfelipe@gmail.com>
2026-05-24 16:31:45 -0300

    create-new-string

---
+++ src/codeforces/strings/create-a-new-string.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    string s1;
+    string s2;
+    cin >> s1 >> s2;
+    cout << s1.size() << ' ' << s2.size() << endl;
+    cout << s1 << ' ' << s2 << endl;
+}
